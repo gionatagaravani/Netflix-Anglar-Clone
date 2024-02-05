b94df4d7b796bef51c7e0eae3a281aa39f803465
Gionata Garavani <gionatagaravani@libero.it>
2024-02-05 18:53:46 +0100

    footer

--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
-
+  footer = ['FAQ', 'Help Center', 'Account', 'Media Center', 'Investor Relations', 'Jobs', 'Redeem Gift Cards', 'Buy Gift Cards', 'Ways to Watch', 'Terms of Use', 'Privacy', 'Cookie Preferences', 'Corporate Information', 'Contact Us', 'Speed Test', 'Legal Guarantee', 'Legal Notices', 'Only on Netflix', 'Ad Choices']
 }
