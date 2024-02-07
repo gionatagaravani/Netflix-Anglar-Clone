5c17da3a85d052b47464d086b3ca2075f4077a74
Gionata Garavani <gionatagaravani@libero.it>
2024-02-08 00:08:28 +0100

    browse

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   footer = [
     'FAQ',
     'Help Center',
@@ -17,8 +18,14 @@ export class LoginComponent {
   ];
   user = { email: '', password: '' };
   loading = false;
-  constructor(private titleService: Title, private auth: AuthService) {
+  constructor(private titleService: Title, private auth: AuthService, private readonly route: Router) {
     this.titleService.setTitle('Netflix');
+  }
+
+  ngOnInit(): void {
+    if(this.auth.isLoggedIn) {
+      this.route.navigate(['/browse']);
+    }
   }
 
   onSubmit(user: any) {
