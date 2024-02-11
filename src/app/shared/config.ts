abc7b12bc62eb782a86a8010082f30a03eb9d6b5
Gionata Garavani <gionatagaravani@libero.it>
2024-02-11 10:37:31 +0100

    tooltip

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,17 +1,17 @@
 export const DB = {
-    URL: "https://netflix-angular-52ee4-default-rtdb.firebaseio.com/"
-}
+  URL: 'https://netflix-angular-52ee4-default-rtdb.firebaseio.com/',
+};
 
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: 'AIzaSyDwYQZRl6i0EDSHnba_QYW7fuasNWaMCdk',
-        authDomain: 'netflix-angular-52ee4.firebaseapp.com',
-        databaseURL: 'https://netflix-angular-52ee4-default-rtdb.firebaseio.com/',
-        projectId: 'netflix-angular-52ee4',
-        storageBucket: 'netflix-angular-52ee4.appspot.com',
-        messagingSenderId: "717305622282",
-        appId: "1:717305622282:web:e8523861ade8bf9c94d4f3",
-        measurementId: "G-JSMF9PQDVB"
-    }
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDwYQZRl6i0EDSHnba_QYW7fuasNWaMCdk',
+    authDomain: 'netflix-angular-52ee4.firebaseapp.com',
+    databaseURL: 'https://netflix-angular-52ee4-default-rtdb.firebaseio.com',
+    projectId: 'netflix-angular-52ee4',
+    storageBucket: 'netflix-angular-52ee4.appspot.com',
+    messagingSenderId: '717305622282',
+    appId: '1:717305622282:web:e8523861ade8bf9c94d4f3',
+    measurementId: 'G-JSMF9PQDVB',
+  },
 };
