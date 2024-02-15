0e7e16e6df47a7e9021584d39dbe67bf04485422
Gionata Garavani <gionatagaravani@libero.it>
2024-02-16 00:01:36 +0100

    ProfileService

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  kind: string;
+  localId: string;
+  email: string;
+  displayName: string;
+  idToken: string;
+  registered: boolean;
+  refreshToken: string;
+  expiresIn: number;
+}
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -3,15 +3,43 @@ import { Injectable } from '@angular/core';
 import { Profile } from '../models/profile';
 import { Observable } from 'rxjs';
 import { DB } from '../shared/config';
+import { AuthService } from './auth.service';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
+  user: User;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly auth: AuthService
+  ) {
+    this.user = auth.getAuthLocal();
+  }
 
-  getProfile(): Observable<Profile> {
-    return this.http.get<Profile>(DB.URL + 'profile.json');
+  getProfiles(): Observable<Profile> {
+    return this.http.get<Profile>(DB.URL + this.user.localId + '/profile.json');
+  }
+
+  getProfile(id: string): Observable<Profile> {
+    return this.http.get<Profile>(
+      DB.URL + this.user.localId + '/profile/' + id + '.json'
+    );
+  }
+
+  createProfile(data: Profile): Observable<any> {
+    return this.http.post(DB.URL + this.user.localId + '/profile.json', data);
+  }
+
+  editProfile(data: Profile): Observable<any> {
+    return this.http.put(DB.URL + this.user.localId + '/profile.json', data);
+  }
+
+  deleteProfile(id: string): Observable<any> {
+    return this.http.delete(
+      DB.URL + this.user.localId + '/profile/' + id + '.json'
+    );
   }
 }
